00dc62ef3b44bf0a6230b4f37f614353f564c87e
zishan344 <zishanahmed344@gmail.com>
2022-02-25 11:23:43 +0600

    done the 2 numbers videos

---
+++ primitive-data-type.js
@@ -0,0 +1,21 @@
+// js 7 data type
+// number data type
+// string data type
+//boolean data type
+//undefined data type
+//null data type
+//object data type
+//symbol data type
+let a = "hello";
+console.log(a);
+let b = "jj";
+console.log(b);
+b = "gelo";
+
+let company = { name: "p-Hero", email: "kdsfjsdgd@gmail.com", work: "web" };
+console.log(company);
+let cp = company;
+console.log(cp);
+cp.work = "web3";
+console.log(company);
+console.log(cp);

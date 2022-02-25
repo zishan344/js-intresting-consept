107b837b0f5602ac6c087a28d03731fe4a66ae7f
zishan344 <zishanahmed344@gmail.com>
2022-02-25 17:27:43 +0600

    done the todays module

--- test.js
+++ test.js
@@ -2,3 +2,14 @@ let p = "Javascript";
 let q = p;
 p = "React";
 console.log(q);
+const isTrue = "false";
+if (!isTrue) {
+  console.log("hello");
+} else {
+  console.log("world");
+}
+function sum(p, q) {
+  p + q;
+}
+const result = sum(2, 3);
+console.log(result);
